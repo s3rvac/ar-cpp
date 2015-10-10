761d6d691620d847aec58c1ab8d2753023e2112a
Petr Zemek <s3rvac@gmail.com>
2015-10-10 09:47:37 +0200

    Ensure that the created temporary files in tests are unique.
    On Windows with MSYS2, there appear to be clashes, even when using
    std::random_device to seed the random number generator.

--- tests/ar/test_utilities/tmp_file.cpp
+++ tests/ar/test_utilities/tmp_file.cpp
@@ -23,9 +23,16 @@ std::string getUniqueTemporaryFilePath() {
 	// As the creation of a temporary file with accessible name in the standard
 	// C++ is painful, simply create the temporary file in the current
 	// directory. After all, this function is only used in tests.
-	std::random_device rd;
-	std::mt19937 mt{rd()};
-	std::uniform_int_distribution<std::size_t> dist;
+	//
+	// The use of static variables makes the code unsafe for threads, but since
+	// we do not use threads in tests, it is OK. The problem is that at least
+	// on Windows (MSYS2), std::random_device is only a pseudo-random number
+	// generator that always generates the same sequence. Therefore, we
+	// initialize the random number generator when the tests start (when
+	// getUniqueTemporaryFilePath() is first called) and use it for all tests.
+	static std::random_device rd;
+	static std::mt19937 mt{rd()};
+	static std::uniform_int_distribution<std::size_t> dist;
 	return "ar-cpp-" + std::to_string(dist(mt)) + ".tmp";
 }
 
